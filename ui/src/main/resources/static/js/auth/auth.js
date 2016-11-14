89de58a040be79b5dba64e77642a299d93558d5f
AlanJager <873863981@qq.com>
2016-11-14 09:18:51 +0800

    add auth js to control auth process

---
+++ ui/src/main/resources/static/js/auth/auth.js
@@ -0,0 +1,86 @@
+angular.module('auth', []).factory(
+    'auth',
+
+    function($rootScope, $http, $location) {
+
+        enter = function() {
+            if ($location.path() != auth.loginPath) {
+                auth.path = $location.path();
+                if (!auth.authenticated) {
+                    $location.path(auth.loginPath);
+                }
+            }
+        }
+
+        var auth = {
+
+            authenticated : false,
+
+            loginPath : '/login',
+            logoutPath : '/logout',
+            homePath : '/',
+            path : $location.path(),
+
+            authenticate : function(credentials, callback) {
+                console.log("authenticate invoked");
+                var headers = credentials && credentials.username ? {
+                    authorization : "Basic "
+                    + btoa(credentials.username + ":"
+                        + credentials.password)
+                } : {};
+
+                $http.get('user', {
+                    headers : headers
+                }).then(function(response) {
+                    console.log("authenticate get success");
+                    if (response.data.name) {
+                        auth.authenticated = true;
+                    } else {
+                        auth.authenticated = false;
+                    }
+                    callback && callback(auth.authenticated);
+                    $location.path(auth.path==auth.loginPath ? auth.homePath : auth.path);
+                }, function() {
+                    console.log("authenticate get failed");
+                    auth.authenticated = false;
+                    callback && callback(false);
+                });
+
+            },
+
+            clear : function() {
+                $location.path(auth.loginPath);
+                auth.authenticated = false;
+                $http.post(auth.logoutPath, {}).then(function() {
+                    console.log("Logout succeeded");
+                }, function() {
+                    console.log("Logout failed");
+                });
+            },
+
+            init : function(homePath, loginPath, logoutPath) {
+
+                auth.homePath = homePath;
+                auth.loginPath = loginPath;
+                auth.logoutPath = logoutPath;
+
+                console.log(loginPath);
+
+                auth.authenticate({}, function(authenticated) {
+                    if (authenticated) {
+                        $location.path(auth.path);
+                    }
+                })
+
+                // Guard route changes and switch to login page if unauthenticated
+                $rootScope.$on('$routeChangeStart', function() {
+                    enter();
+                });
+
+            }
+
+        };
+
+        return auth;
+
+    });
